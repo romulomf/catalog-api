f70f45a49583392765f29c09eed7a0d3bb59132a
Rômulo Machado Flores <romulo@tag.net.br>
2025-04-24 22:46:13 -0300

    Setup dos testes unitários
    Adição do projeto de testes unitários e reorganizado o projeto da web api

---
+++ CatalogApiTests/ProductControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using CatalogApi.Controllers;
+
+namespace CatalogApiTests;
+
+public class ProductControllerTest
+{
+	private readonly ProductController? _controller;
+
+	public ProductControllerTest()
+	{
+		var services = new ServiceCollection();
+		services.AddTransient<ProductController>();
+		var provider = services.BuildServiceProvider();
+		_controller = provider.GetService<ProductController>();
+	}
+
+	[Fact]
+	public void Test1()
+	{
+
+	}
+}
