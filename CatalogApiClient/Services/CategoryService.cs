b1cd04eaca94e035c17b0bb518ff368c357377f7
Rômulo Machado Flores <romulo@tag.net.br>
2025-07-11 17:31:58 -0300

    Atualização de dependências

---
+++ CatalogApiClient/Services/CategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace CatalogApiClient.Services;
+
+public class CategoryService(IHttpClientFactory httpClientFactory)
+{
+	private readonly JsonSerializerOptions _jsonSerializerOptions = new()
+	{
+		PropertyNameCaseInsensitive = true,
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+	};
+
+	private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+}
